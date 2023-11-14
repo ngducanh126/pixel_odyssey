a90bb2144ddabfeefb07c6d5af901424d399e3d7
quyvan01 <quyvan20012005@gmail.com>
2023-11-14 14:50:55 -0500

    changes

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -2,55 +2,48 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    public GameObject fireballPrefab; // Assign this in the inspector with your fireball prefab
-    public float fireballSpeed = 10f; // Adjust the speed as needed
+    public GameObject fireballPrefab; 
+    public float fireballSpeed = 10f; 
+    public float maxDistance = 10f; // Set your desired maximum distance
     [SerializeField] private Transform firePoint;
 
     private Animator animator;
-    private SpriteRenderer spriteRenderer; // Add this
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
-        // Get the Animator and SpriteRenderer components attached to the player
         animator = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>(); // Get the SpriteRenderer component
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        // Check if the 'S' key is pressed
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            // Trigger the attack animation
             animator.SetTrigger("attack");
 
-            // Check the direction the player is facing to determine the firePoint position
             float directionMultiplier = spriteRenderer.flipX ? -1f : 1f;
             firePoint.localPosition = new Vector3(Mathf.Abs(firePoint.localPosition.x) * directionMultiplier, firePoint.localPosition.y, firePoint.localPosition.z);
 
-            // Instantiate the fireball at the firePoint's position
             GameObject fireballInstance = Instantiate(fireballPrefab, firePoint.position, Quaternion.identity);
 
-            // If shooting to the left, flip the fireball's sprite to face left
             if (spriteRenderer.flipX)
             {
-                fireballInstance.transform.localScale = new Vector3(-1 * Mathf.Abs(fireballInstance.transform.localScale.x), 
-                                                                    fireballInstance.transform.localScale.y, 
+                fireballInstance.transform.localScale = new Vector3(-1 * Mathf.Abs(fireballInstance.transform.localScale.x),
+                                                                    fireballInstance.transform.localScale.y,
                                                                     fireballInstance.transform.localScale.z);
             }
 
-            // Determine the direction to fire the fireball based on the sprite's flipX
             Vector2 fireDirection = spriteRenderer.flipX ? Vector2.left : Vector2.right;
 
-            // Make the fireball move in the determined direction
             Rigidbody2D rb = fireballInstance.GetComponent<Rigidbody2D>();
             if (rb != null)
             {
                 rb.velocity = fireDirection * fireballSpeed;
+
+                // Destroy the fireball after it travels a certain distance
+                Destroy(fireballInstance, maxDistance / fireballSpeed);
             }
         }
     }
-
-
-
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,19 +12,14 @@ public class PlayerMovement : MonoBehaviour
     private float dirX = 0f;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 2f;
-    [SerializeField] private float fireSpeed = 2f;
+
 
     [SerializeField] private LayerMask jumpableGround;
 
-    public GameObject firePrefab; 
-    public Vector2 fireOffset = new Vector2(1.0f, 0);
-
-    private float fireCooldown = 2f; // Cooldown duration in seconds
-    private float lastFireTime = -2f; // Initialize to allow firing immediately at start
 
     private enum MovementState { idle, running, jumping, falling }
 
-    // Start is called before the first frame update
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -44,20 +39,7 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
-        if (Input.GetKeyDown(KeyCode.F) && Time.time - lastFireTime >= fireCooldown)
-        {
-            SpawnFire();
-            lastFireTime = Time.time; // Update the last fire time to the current time
-        }
-
        UpdateAnimationState();
-    }
-
-    void SpawnFire()
-    {
-        // Spawn the fire object at the player's position + offset
-        GameObject fireInstance = Instantiate(firePrefab, (Vector2)transform.position + fireOffset, Quaternion.identity);
-        fireInstance.GetComponent<Rigidbody2D>().velocity = new Vector2(fireSpeed, 0); // Set the velocity of the spawned fire
     }
 
        private void UpdateAnimationState()
