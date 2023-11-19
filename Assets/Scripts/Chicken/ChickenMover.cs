4cc4a8be5d8a455e4773d5bf46a4866efa21319f
quyvan01 <quyvan20012005@gmail.com>
2023-11-19 18:48:52 -0500

    deded

--- Assets/Scripts/Chicken/ChickenMover.cs
+++ Assets/Scripts/Chicken/ChickenMover.cs
@@ -13,11 +13,6 @@ public class ChickenMover : MonoBehaviour
 
     private void Update()
     {
-        if(target) {
-            GetComponent<Animator>().SetBool("seePlayer", true);
-        } else {
-            GetComponent<Animator>().SetBool("seePlayer", false);
-        }
 
         if(!target){
 
@@ -36,23 +31,5 @@ public class ChickenMover : MonoBehaviour
         transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
         }
 
-    }
-
-    void OnTriggerStay2D(Collider2D c) {
-        if(c.gameObject.tag == "Player") {
-            Vector2 newPos = new Vector2(c.gameObject.transform.position.x, transform.position.y);
-            transform.position = Vector2.MoveTowards(transform.position, newPos, Time.deltaTime * speed * 2);
-            Vector2 dir = c.gameObject.transform.position - transform.position;
-            GetComponent<Animator>().SetFloat("DirX", dir.x);
-            target = true;
-        } else {
-            target = false;
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D c) {
-        if(c.gameObject.tag == "Player") {
-            target = false;
-        }
     }
 }
