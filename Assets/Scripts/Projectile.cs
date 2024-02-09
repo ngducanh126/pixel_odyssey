8eb4117f3e523ad0bbc85ee2b7077f80c4f28342
anh <ngducanh126@gmail.com>
2024-02-08 20:18:31 -0500

    Projectiles now bounce off walls for ricochet effect

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour {
-    public AudioClip impactSound;
+    public int bounceCount = 1;
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.collider.CompareTag("Wall") && bounceCount > 0) {
+            speed = -speed;
+            bounceCount--;
+        }
+    }
+        public AudioClip impactSound;
     public void PlayImpactSound() {
         if (impactSound != null) AudioSource.PlayClipAtPoint(impactSound, transform.position);
     }
