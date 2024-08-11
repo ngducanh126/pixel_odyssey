0e6a7fa885ada2172fe6f6b5b038955ab45736b5
anh <ngducanh126@gmail.com>
2024-08-10 22:22:07 -0400

    Chicken now reacts to trap triggers with instant destruction

--- Assets/Scripts/ChickenHit.cs
+++ Assets/Scripts/ChickenHit.cs
@@ -2,7 +2,13 @@ using UnityEngine;
 
 public class ChickenHit : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Trap")) {
+            Destroy(gameObject);
+            Debug.Log("Chicken destroyed by trap");
+        }
+    }
+        private void OnCollisionEnter2D(Collision2D collision)
     {
         // Check if the collided object has the "Fireball" tag
         if (collision.gameObject.CompareTag("Fireball"))
