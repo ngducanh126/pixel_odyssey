5b453d4b1aea95935b322da82e0c32e21e8cc0ef
anh <ngducanh126@gmail.com>
2024-12-21 23:12:51 -0500

    Coin prefab can now be set from script

--- Assets/Scripts/TerrainSpawner.cs
+++ Assets/Scripts/TerrainSpawner.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class TerrainSpawner : MonoBehaviour
 {
-    public void ReduceSpawnCooldown(float amount) {
+    public void SetCoinPrefab(GameObject prefab) {
+        coinPrefab = prefab;
+        Debug.Log("Coin prefab set from script");
+    }
+        public void ReduceSpawnCooldown(float amount) {
         spawnRate = Mathf.Max(0.1f, spawnRate - amount);
         CancelInvoke(nameof(SpawnTerrain));
         InvokeRepeating(nameof(SpawnTerrain), 0, spawnRate);
